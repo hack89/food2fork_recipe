fd5f8d943ff8dd8eb183c592878b53b6d5b23ca3
hack89 <hackinsta89@gmail.com>
2019-10-28 17:21:16 +0100

    Updates

---
+++ precache-manifest.51cf2ed488c4e28c1c75e513ec15ca0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4d75a62609f868ef567ee23b582343",
+    "url": "/food2fork_recipe/index.html"
+  },
+  {
+    "revision": "07a15c423aa1c4e79e50",
+    "url": "/food2fork_recipe/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "07c8f017dd61cdec3d09",
+    "url": "/food2fork_recipe/static/js/2.ebce2b6b.chunk.js"
+  },
+  {
+    "revision": "07a15c423aa1c4e79e50",
+    "url": "/food2fork_recipe/static/js/main.04309e12.chunk.js"
+  },
+  {
+    "revision": "e8f31fedd48ef0e0c1a8",
+    "url": "/food2fork_recipe/static/js/runtime~main.68add553.js"
+  }
+]);
